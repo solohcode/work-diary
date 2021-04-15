3d4a0b01d654963f32ae54831bb11da5e909819c
solomon ayodele <solomonayodele65@gmail.com>
2021-04-15 16:44:08 +0100

    update

--- src/Components/Pages/Landing/Top.js
+++ src/Components/Pages/Landing/Top.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Button} from 'antd'
+import {Button, Popover} from 'antd'
 import { UserOutlined} from '@ant-design/icons';
 
 
@@ -9,6 +9,9 @@ import w2 from '../../../Images/banner/w1.png'
 import w3 from '../../../Images/banner/w2.png'
 import w4 from '../../../Images/banner/w4.png'
 import w5 from '../../../Images/banner/w5.png'
+import w6 from '../../../Images/banner/w7.png'
+import w7 from '../../../Images/banner/w8.png'
+import w8 from '../../../Images/banner/w9.png'
 import logo from '../../../Images/logos/icon01.png'
 import side from '../../../Images/banner/w6.png'
 
@@ -16,13 +19,37 @@ import side from '../../../Images/banner/w6.png'
 
 export class Top extends Component {
     render() {
+        const content =[
+            (
+            <div>
+              <span>Create a work diary account <br/> to get experienced what it is all about. </span>
+            </div>
+             ),
+             (
+            <div>
+                <span>Set up your work diary dashboard <br/> with any layout of your choice. </span>
+            </div>
+             ),
+             (
+            <div>
+                <span>Upload your work or business datas, <br/> information and portfolios. </span>
+            </div>
+            ),
+            (
+            <div>
+                <span>Sit back and watch your dashboard <br/> bring you your desired job and clients. </span>
+            </div>
+            ),
+
+        ];
+
         return (
             <div>
                 <div className="wd-top bg-white">
                     <br/>
                     <div class="shadow-sm container-fluid mt-4" style={{height:'100%'}}>
                         <div class="row">
-                            <div class="col-md-6 text-center border-bottom ">
+                            <div class="col-md-5 text-center border-bottom ">
                                 <div class="mt-big ">
                                     <br/>
                                     <img src={logo} class="mt-5 animate-top" alt="icon.." width="50px" height="50px" />
@@ -51,11 +78,41 @@ export class Top extends Component {
                                 </div>
                             </div>
 
-                            <div className="wd-top-side d-none d-md-block col-md-6 border-bottom " >
+                            <div className="wd-top-side d-none d-md-block col-md-7 border-bottom " >
                                 <div class="w-100 h-75">
+                                    <br/>
+                                    <div className="text-center mt-5 size-big p-5">
+                                        <span className="fw-light text-gray">FEW STEPS TO </span>
+                                        <br/>
+                                        <span className="text-light fst-italic ">GETTING YOUR WORK OR YOUR BUSINESS RECOGNIZED</span>
+                                    </div>
                                 </div>
                                 <div className="h-25 top-100 bottom-0 bg-dark w-100 animate-opacity text-center">
-                                        <img src={side} alt="let your portfolio dashboard get you your preferred job" className="mx-auto" width="200px" height="100px" style={{marginTop:'-100px'}}/>
+                                    <br/>
+                                    <div className="container-fluid">
+                                        <div style={{marginTop:'-100px'}} className="row">
+                                            <div className="col-3">
+                                                <Popover content={content[0]} title="Create An Account">
+                                                    <img src={side} alt="..." width="100%" height="100px"/>
+                                                </Popover>
+                                            </div>
+                                            <div className="col-3">
+                                                <Popover content={content[1]} title="Set Up Dashboard">
+                                                    <img src={w8} alt="..." width="100%" height="100px"/>
+                                                </Popover>
+                                            </div>
+                                            <div className="col-3">
+                                                <Popover content={content[2]} title="Upload Portfolios">
+                                                    <img src={w6} alt="..." width="100%" height="100px"/>
+                                                </Popover>
+                                            </div>
+                                            <div className="col-3">
+                                                <Popover content={content[3]} title="Get Hired">
+                                                    <img src={w7} alt="..." width="100%" height="100px"/>
+                                                </Popover>
+                                            </div>
+                                        </div>
+                                    </div>
                                         <div className="w-75 text-center text-gray fst-italic mx-auto mt-2">
                                             <span>Work Diary is an online portfolio reviewer designed to hold any job seeker or worker of any organization or a business entrepreneur his or her work done, their portfolios to be precise, for review purpose. 
                                                 <br/>
@@ -69,7 +126,7 @@ export class Top extends Component {
                                 </div>
                             </div>
 
-                            <div className="w-75 text-center mx-auto d-none d-md-block col-md-6" >
+                            <div className="w-75 text-center mx-auto d-none d-md-block" >
                                  <div class=" animate-opacity">
                                         <img src={w1} alt="my work dashboard" width="100%" height="100%" />
                                         <span className="text-silver fst-italic"><i className="fas fa-stream"/> my work dashboard</span>
